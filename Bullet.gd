cb48ec885c514a7964f2d631a9a7af0c1500379f
Brandon Blodget <brandon@ologicinc.com>
2020-07-27 19:26:01 -0700

    Adding bullets.  Buggy!!

---
+++ Bullet.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+
+# Max speeds
+const BULLET_SPEED = 40
+
+# Global Vars
+var velocity = Vector2.ZERO
+var heading = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	velocity.y = -100
+
+func set_heading(new_heading):
+	heading = new_heading
+
+func _physics_process(delta):
+
+	#velocity.x = delta * BULLET_SPEED * cos(heading * (PI/180))
+	#velocity.y = delta * BULLET_SPEED * sin(heading * (PI/180))
+	#move_and_collide(velocity)
+	
+	if position.y < -1000:
+		queue_free()
+		
+	move_and_slide(velocity)
--- TankPlayer.gd
+++ TankPlayer.gd
@@ -17,31 +17,50 @@ export var ui_left : String
 export var ui_right : String
 export var ui_up : String
 export var ui_down : String
+export var ui_fire : String
 export var initial_heading : float = 0.0
 
 func _ready():
 	heading = get_rotation_degrees()
-	print("heading: ", heading)	
+
 	$tank.texture = tank_sprite
 
 func _physics_process(delta):
+	var fire = Input.get_action_strength(ui_fire)
+	
+	if fire:
+		shoot()	
+
+		
 	var turn = TURN_SPEED * (Input.get_action_strength(ui_right) - Input.get_action_strength(ui_left))	
 	
 	heading = heading + (turn * delta)
 	
 	if turn != 0:
 		set_rotation_degrees(heading)
-		print("heading: ", heading)
+
 		
 	var fwd = FWD_SPEED * delta * (Input.get_action_strength(ui_down) - Input.get_action_strength(ui_up))	
 	
 	if fwd !=0:
-		var adj_heading = heading+90 - initial_heading
+		var adj_heading = heading+90 + initial_heading
 		velocity.x = fwd * cos(adj_heading * (PI/180))
 		velocity.y = fwd * sin(adj_heading * (PI/180))
-		print("velocity: ", velocity)
+
 	else:
 		velocity = Vector2.ZERO
 	
 	move_and_collide(velocity)
+	
+
+	
+func shoot():
+	var bullet_scene = load("res://Bullet.tscn")
+	var bullet = bullet_scene.instance()
+	var world = get_tree().get_root().get_node("World")
+	
+	bullet.transform = world.transform
+	add_child_below_node(world, bullet)
+	
+	
 	
