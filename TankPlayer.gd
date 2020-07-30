63934a059015e77053fc727b99ccc99b02fca61f
Brandon Blodget <brandon@ologicinc.com>
2020-07-29 18:19:56 -0700

    Fire Offset is working.

--- TankPlayer.gd
+++ TankPlayer.gd
@@ -38,7 +38,7 @@ func _physics_process(delta):
 	var turn = TURN_SPEED * (Input.get_action_strength(ui_right) - Input.get_action_strength(ui_left))	
 	
 	heading = heading + (turn * delta)
-	adj_heading = heading+90 + initial_heading
+	adj_heading = heading + initial_heading
 	
 	if turn != 0:
 		set_rotation_degrees(heading)
@@ -52,19 +52,18 @@ func _physics_process(delta):
 	var fwd = FWD_SPEED * delta * (Input.get_action_strength(ui_down) - Input.get_action_strength(ui_up))	
 	
 	if fwd !=0:
-		velocity.x = fwd * cos(adj_heading * (PI/180))
-		velocity.y = fwd * sin(adj_heading * (PI/180))
+		velocity.x = (-fwd) * sin(adj_heading * (PI/180))
+		velocity.y = fwd * cos(adj_heading * (PI/180))
 
 	else:
 		velocity = Vector2.ZERO
 	
 	move_and_collide(velocity)
 	
-
 	
 func shoot():
 	
-	var direction = Vector2(cos(adj_heading * (PI/180)), sin(adj_heading * (PI/180)))
+	var direction = Vector2(sin(adj_heading * (PI/180)), -cos(adj_heading * (PI/180)))
 	
 	var distance_from_me = 10
 	var gpos = get_global_position() 
