eb03ff32d3e3b518468e6a6092161c2cefa69ed8
Brandon Blodget <brandon@ologicinc.com>
2020-08-09 16:03:12 -0700

    Game Over message in place.

--- HUD.gd
+++ HUD.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
+# Global Vars
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
+# Export vars
+export var win_score : int
+export var GREEN : Color = Color(0.19, 0.67, 0.17, 1.0)
+export var ORANGE : Color = Color(0.83, 0.72, 0.16, 1.0)
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -29,4 +33,16 @@ func _on_tank_hit(dead_tank, live_tank):
 		score_board.text = "0" + str(score)
 	else:
 		score_board.text = str(score)
+		
+	if score == win_score:
+		if live_tank == "Tank1":
+			#$PlayerWins.set("custom_colors/font_color",GREEN)
+			$PlayerWins.add_color_override("font_color", GREEN)
+			$PlayerWins.text = "Green Wins"
+		else:
+			#$PlayerWins.set("custom_colors/font_color",ORANGE)
+			$PlayerWins.add_color_override("font_color", ORANGE)
+			$PlayerWins.text = "Orange Wins"
+		$PlayerWins.show()
+		$GameOver.show()
 
