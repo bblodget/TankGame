5a69307cc75d5bc335fe708c4b984299c2f41a93
Brandon Blodget <brandon@ologicinc.com>
2020-09-16 15:42:07 -0700

    Started on the TitleScreen Scene.
    Boat Splits the text.

---
+++ TitleBoat1.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+
+# Max speeds
+const FWD_SPEED = 50
+
+# Global Vars
+var velocity = Vector2.ZERO
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _physics_process(delta):
+
+	# Move Up
+	velocity.y = -FWD_SPEED * delta 
+	
+	move_and_collide(velocity)
+	
+
+
+func _on_Screen_Exit_body_entered(body):
+	if body == self:
+		print("Delete Boat1")
+		queue_free()
---
+++ TitleScreen.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Max speeds
+const FWD_SPEED = 10
+
+# Global Vars
+var velocity = Vector2.ZERO
+
+var move_text = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var x
+	if move_text == true:
+		velocity.x = FWD_SPEED * delta 
+		
+		x = $at_label.rect_position.x
+		$at_label.rect_position.x = x + velocity.x
+		
+		x = $Comb_label.rect_position.x
+		$Comb_label.rect_position.x = x - velocity.x
+
+
+func _on_Title_Area_body_entered(body):
+	print("Boat entered Title Area")
+	move_text = true
+
+func _on_Title_Area_body_exited(body):
+	print("Boat exited Title Area")
+	move_text = false
